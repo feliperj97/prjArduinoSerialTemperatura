030931421e4b8ca9638c9c692cc9db5403cbbc0e
Felipe Lamim <fclamim@gmail.com>
2018-06-01 21:12:50 -0300

    primeiro commit

---
+++ SupervisorioSerial.ino
@@ -0,0 +1,31 @@
+#include "DHT.h"
+
+DHT dht(3, DHT11);
+String tipoDado = "$STS";
+String separador = ",";
+float temperatura = 0;
+float umidade = 0;
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  Serial.println("Iniciando leitura...");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(500);
+  temperatura = dht.readTemperature();
+  umidade = dht.readHumidity();
+
+  enviaDadosSerial();
+
+}
+void enviaDadosSerial(){
+    Serial.print(tipoDado);
+    Serial.print(separador);
+    Serial.print(temperatura);
+    Serial.print(separador);
+    Serial.print(umidade);
+    Serial.println();
+  }
