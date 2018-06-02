b803ea4b10d94d41937bc2f96ac9f1adb764c048
Felipe Lamim <fclamim@gmail.com>
2018-06-01 21:22:51 -0300

    exibindo apenas temperatura

--- SupervisorioSerial.ino
+++ SupervisorioSerial.ino
@@ -1,10 +1,10 @@
 #include "DHT.h"
 
 DHT dht(3, DHT11);
-String tipoDado = "$STS";
-String separador = ",";
+//String tipoDado = "$STS";
+//String separador = ",";
 float temperatura = 0;
-float umidade = 0;
+//float umidade = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -14,18 +14,18 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(500);
+  delay(1000);
   temperatura = dht.readTemperature();
-  umidade = dht.readHumidity();
+ // umidade = dht.readHumidity();
 
   enviaDadosSerial();
 
 }
 void enviaDadosSerial(){
-    Serial.print(tipoDado);
-    Serial.print(separador);
-    Serial.print(temperatura);
-    Serial.print(separador);
-    Serial.print(umidade);
-    Serial.println();
+   // Serial.print(tipoDado);
+    //Serial.print(separador);
+    Serial.println(temperatura);
+    //Serial.print(separador);
+    //Serial.print(umidade);
+    //Serial.println();
   }
